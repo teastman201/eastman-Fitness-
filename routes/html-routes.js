9b73b833733428cc8cbb761e78cf216c58d666cb
teastman201 <teastman85@gmail.com>
2020-11-23 08:25:34 -0500

    Clean up html-routes file.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,40 +1,20 @@
-// *********************************************************************************
-// html-routes.js - this file offers a set of routes for sending users to the various html pages
-// *********************************************************************************
-
 const path = require('path');
 const app = require("express").Router();
 
-// middleware that is specific to this router
-// router.use(function timeLog(req, res, next) {
-//   console.log('Time: ', Date.now())
-//   next()
-// })
-
-// define the home page route
+// route for home page
 app.get("/"), function (req, res) {
   res.sendFile(path.join(__dirname, "../public/exercise.html"))
 }
 
+// route for new workout
 app.get('/exercise', function (req, res) {
   console.log()
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
-  // res.send('Birds home page')
 })
 
+// route for dashboard
 app.get("/stats", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/stats.html"))
 })
-
-
-// const router = express.Router();
-// app.use(router);
-
-// // Routes
-// router.get("/exercise", function (req, res) {
-//   console.log("exerciseRoute");
-//   res.sendFile(express.static(__dirname, "/exercise.html"));
-
-// });
 
 module.exports = app;
