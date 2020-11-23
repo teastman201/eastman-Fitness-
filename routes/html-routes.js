d8512964ebd3714cabb7046f702a8bb36d0a2bf8
teastman201 <teastman85@gmail.com>
2020-11-23 06:58:04 -0500

    Add html-routes.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,18 +3,18 @@
 // *********************************************************************************
 
 const path = require('path');
-var express = require('express')
-var router = express.Router()
+var router = require('express').Router()
 
 // middleware that is specific to this router
-router.use(function timeLog(req, res, next) {
-  console.log('Time: ', Date.now())
-  next()
-})
+// router.use(function timeLog(req, res, next) {
+//   console.log('Time: ', Date.now())
+//   next()
+// })
+
 // define the home page route
 router.get('/exercise', function (req, res) {
   console.log()
-  res.sendFile(express.static(__dirname, "/exercise.html"));
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
   // res.send('Birds home page')
 })
 
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Brings in required packages
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
@@ -6,13 +7,13 @@ const mongoose = require("mongoose");
 // using 8080 because 3000 is being used by comparison test environment.
 const PORT = process.env.PORT || 8080;
 
+// Creates express server
 const app = express();
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
+// Creates MongoDB database connection
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
     useNewURLParser: true,
     useFindAndModify: false,
@@ -20,8 +21,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
 });
 
 // routes
-// app.use(require("./routes/index"))
+app.use(require("./routes/api-routes"))
+app.use(require("./routes/html-routes"))
 
+// When server starts listening, it indicates which port.
 app.listen(PORT, () => {
     console.log(`Appp running on port ${PORT}!`)
 })
