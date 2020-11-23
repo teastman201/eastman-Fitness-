7e25ccb68d87afa87ce7df8800e4a7c88f1147a9
teastman201 <teastman85@gmail.com>
2020-11-23 08:47:57 -0500

    Workign post and put api routes.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -20,34 +20,24 @@ app.post("/api/workouts", function (req, res) {
     })
 })
 
-// app.get("/api/authors/:id", function (req, res) {
-//   // Here we add an "include" property to our options in our findOne query
-//   // We set the value to an array of the models we want to include in a left outer join
-//   // In this case, just db.Post
-//   db.Author.findOne({
-//     where: {
-//       id: req.params.id
-//     },
-//     include: [db.Post]
-//   }).then(function (dbAuthor) {
-//     res.json(dbAuthor);
-//   });
-// });
-
-// app.post("/api/authors", function (req, res) {
-//   db.Author.create(req.body).then(function (dbAuthor) {
-//     res.json(dbAuthor);
-//   });
-// });
-
-// app.delete("/api/authors/:id", function (req, res) {
-//   db.Author.destroy({
-//     where: {
-//       id: req.params.id
-//     }
-//   }).then(function (dbAuthor) {
-//     res.json(dbAuthor);
-//   });
-// });
+app.put("/api/workouts/:id", function (req, res) {
+  // Here we add an "include" property to our options in our findOne query
+  // We set the value to an array of the models we want to include in a left outer join
+  // In this case, just db.Post
+  db.Workout.findByIdAndUpdate(
+    req.params.id,
+    {
+      $push:
+      {
+        exercises: req.body
+      }
+    },
+    {
+      new: true
+    }
+  ).then(function (dbWorkout) {
+    res.json(dbWorkout);
+  });
+});
 
 module.exports = app 
